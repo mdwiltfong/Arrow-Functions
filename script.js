047e14d57a7ea9d184d9627c6ab364bc97434ff6
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2021-08-22 01:35:30 -0400

    final commit

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const double=(arr)=>arr.map((val)=>val*2)
 console.log(double(arr))
 
 
-function squareAndFindEvens(numbers){
-    var squares = numbers.map(function(num){
-      return num ** 2;
-    });
-    var evens = squares.filter(function(square){
-      return square % 2 === 0;
-    });
-    return evens;
+const squareAndFindEvens=(numbers)=>{
+    var squares = numbers.map((num)=>num ** 2);
+    var evens = squares.filter((square)=> square%2 === 0);
+    return evens
   }
+  arr=[3,4,5,6]
+  
+  
+  squareAndFindEvens(arr)
